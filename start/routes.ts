e90917e5810045586adf4bfc751e4dca74b2d025
vocafeuvre <hirafura96@gmail.com>
2024-06-01 20:17:26 +0300

    fix: set domain as real host env

--- start/routes.ts
+++ start/routes.ts
@@ -8,12 +8,18 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import env from './env.js'
 
 router.get('/', async ({ request, view, response }) => {
   const allCookies = request.qs()
 
   for (let [key, value] of Object.entries(allCookies)) {
-    response.plainCookie(key, value, { encode: false })
+    response.plainCookie(key, value, {
+      encode: false,
+      domain: env.get('REAL_HOST') || undefined,
+      path: '/',
+      maxAge: 365 * 24 * 60 * 60,
+    })
   }
 
   return view.render('pages/home')
